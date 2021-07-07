d3c86408f1f2007d168ee936b10f9a834dc11e1c
Kabore Lucien <dikize@icloud.com>
2021-07-08 00:28:49 +0200

    mise en place des fichiers models controllers routes & fichiers sauce, user, app et server js & creation model sauce pour le stockage & debut des routes

---
+++ back-end/routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+const sauce = require('../controllers/sauce');
+
+
+// Routes
+router.get('/', );
+router.get('/:id', );
+router.post('/', );
+router.put('/:id', );
+router.delete('/:id', );
+router.post('/:id/like', ); 
+
+module.exports = router;
